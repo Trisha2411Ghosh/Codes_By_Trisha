2bc4147e28aa5a686531da13d23bb4f4cb9c33a7
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-06-07 16:31:27 +0530

    Time: 8 ms (59.21%), Space: 14 MB (76.49%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        // vector<int> arr;
+        // ListNode* temp=head;
+        // if(head==NULL || head->next == NULL){
+        //     return head;
+        // }
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp!=NULL){
+        // arr.push_back(temp->val);
+        // }
+        // temp=head->next;
+        // while(temp!=NULL && temp->next!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next->next;
+        // }
+        // if(temp!=NULL){
+        // arr.push_back(temp->val);
+        // }
+        // temp=head;
+        // for(int i=0;i<arr.size();i++){
+        //     temp->val=arr[i];
+        //     temp=temp->next;
+        // }
+        // return head;
+        if(head==NULL || head->next == NULL || head->next->next==NULL){
+            return head;
+        }
+        ListNode* odd=head;
+        ListNode* even = head->next;
+        ListNode* evenhead=head->next;
+        while(even!=NULL && even->next!=NULL){
+            odd->next=odd->next->next;
+            even->next=even->next->next;
+
+            odd=odd->next;
+            even=even->next;
+        }
+        odd->next=evenhead;
+        return head;
+    }
+};
