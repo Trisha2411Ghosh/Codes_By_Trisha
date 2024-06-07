077c795c4f1f1d334f63cc1c4c4c5b277e346723
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-06-07 16:39:30 +0530

    Time: 3 ms (70.78%), Space: 11 MB (60.15%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        sort(strs.begin(),strs.end());
+        string ans="";
+        string first=strs[0],last=strs[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+            }
+            ans+=first[i];
+        }
+        return ans;
+    }
+};
