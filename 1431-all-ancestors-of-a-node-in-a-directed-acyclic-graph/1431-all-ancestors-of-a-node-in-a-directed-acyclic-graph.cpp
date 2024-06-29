edb56401ffeeebfdb227b0140c220a3654b2ed9e
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-06-29 11:38:05 +0530

    Time: 97 ms (91.33%), Space: 63.2 MB (91.22%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<vector<int>> ans(n);
+
+        // Build the adjacency list
+        for (int i = 0; i < edges.size(); i++) {
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+
+        // Helper function to perform DFS
+        function<void(int, int)> dfs = [&](int node, int ancestor) {
+            for (int child : adj[node]) {
+                if (ans[child].empty() || ans[child].back() != ancestor) {
+                    ans[child].push_back(ancestor);
+                    dfs(child, ancestor);
+                }
+            }
+        };
+
+        // Find ancestors for each node
+        for (int i = 0; i < n; ++i) {
+            dfs(i, i);
+        }
+
+        return ans;
+    }
+};
