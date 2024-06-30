2a064aeee124827f665f27d4f9620af231530392
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-06-30 21:09:59 +0530

    Time: 14 ms (28.3%), Space: 22.3 MB (81.76%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,53 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int leftMax = height[left];
-        int rightMax = height[right];
-        int water = 0;
+        int n = height.size();
+        int l = 0 ;
+        int r = n-1;
+        int leftmax = 0 ;
+        int rightmax = 0 ;
+        int res = 0 ;
 
-        while (left < right) {
-            if (leftMax < rightMax) {
-                left++;
-                leftMax = max(leftMax, height[left]);
-                water += leftMax - height[left];
-            } else {
-                right--;
-                rightMax = max(rightMax, height[right]);
-                water += rightMax - height[right];
+        while (l<r){
+            if(height[l] <= height[r]){
+                if (leftmax < height[l]){
+                    leftmax = height[l];
+                }
+                else{
+                    res += (leftmax - height[l]);
+                }
+                l++;
+            }
+            else{
+                if (rightmax <= height[r]){
+                    rightmax = height[r];
+                }
+                else{
+                    res += (rightmax - height[r]);
+                }
+                r--;
             }
         }
+        return res;
 
-        return water;
+        // int left = 0;
+        // int right = height.size() - 1;
+        // int leftMax = height[left];
+        // int rightMax = height[right];
+        // int water = 0;
+
+        // while (left < right) {
+        //     if (leftMax < rightMax) {
+        //         left++;
+        //         leftMax = max(leftMax, height[left]);
+        //         water += leftMax - height[left];
+        //     } else {
+        //         right--;
+        //         rightMax = max(rightMax, height[right]);
+        //         water += rightMax - height[right];
+        //     }
+        // }
+
+        // return water;
     }
 };
