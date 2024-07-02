d0f0f510a3341fd3aa1e29021ac0bc0abce9c2fb
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-07-02 14:10:05 +0530

    Time: 0 ms (100%), Space: 12.6 MB (59.7%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m = nums2.size();
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
         vector<int> ans;
-        unordered_map<int,int> count;
-
-        for(int i=0;i<n;i++){
-            count[nums1[i]]++;
-        }
-
-        for(int j=0;j<m;j++){
-            if(count[nums2[j]]>0){
-                ans.push_back(nums2[j]);
-                count[nums2[j]]--;
+        int i = 0, j = 0;
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
             }
         }
+        
         return ans;
+
+        // int n=nums1.size();
+        // int m = nums2.size();
+        // vector<int> ans;
+        // unordered_map<int,int> count;
+
+        // for(int i=0;i<n;i++){
+        //     count[nums1[i]]++;
+        // }
+
+        // for(int j=0;j<m;j++){
+        //     if(count[nums2[j]]>0){
+        //         ans.push_back(nums2[j]);
+        //         count[nums2[j]]--;
+        //     }
+        // }
+        // return ans;
 
         // vector<int> ans;
         // int n=nums1.size();
