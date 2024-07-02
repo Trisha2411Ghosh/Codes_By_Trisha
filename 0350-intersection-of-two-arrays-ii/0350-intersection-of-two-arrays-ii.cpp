82aa7fe28567e289e6eb2fdacc2e2ea8fc575bbe
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-07-02 10:43:44 +0530

    Time: 6 ms (32.8%), Space: 14.3 MB (7.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
         int n=nums1.size();
         int m = nums2.size();
-        vector<bool> used(n, false);
-            for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    if(nums1[j]==nums2[i] && !used[j]){
-                        ans.push_back(nums1[j]);
-                        used[j] = true;
-                        break;
-                    }
-                }
+        vector<int> ans;
+        unordered_map<int,int> count;
+
+        for(int i=0;i<n;i++){
+            count[nums1[i]]++;
+        }
+
+        for(int j=0;j<m;j++){
+            if(count[nums2[j]]>0){
+                ans.push_back(nums2[j]);
+                count[nums2[j]]--;
             }
+        }
         return ans;
+
+        // vector<int> ans;
+        // int n=nums1.size();
+        // int m = nums2.size();
+        // vector<bool> used(n, false);
+        //     for(int i=0;i<m;i++){
+        //         for(int j=0;j<n;j++){
+        //             if(nums1[j]==nums2[i] && !used[j]){
+        //                 ans.push_back(nums1[j]);
+        //                 used[j] = true;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // return ans;
     }
 };
