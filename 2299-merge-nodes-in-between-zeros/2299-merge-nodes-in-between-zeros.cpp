317afd5e74a92df04f66d1eae0f40d181f5434aa
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-07-04 11:18:41 +0530

    Time: 462 ms (34.44%), Space: 278.2 MB (18.87%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -11,20 +11,39 @@
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* current = dummy;
-        ListNode* node=head->next;
-        while(node!=NULL){
-            int sum = 0;
-            while(node->val!=0){
-                sum+=node->val;
-                node=node->next;
+        ListNode* current = head->next; 
+        ListNode* prev = head;
+        int sum = 0;
+        while (current != NULL) {
+            if (current->val == 0) {
+                if (sum > 0) {
+                    prev->next = new ListNode(sum);
+                    prev = prev->next;
+                    sum = 0;
+                }
+            } else {
+                sum += current->val;
             }
-            current->next=new ListNode(sum);
-            current=current->next;
-            node=node->next;
+            current = current->next;
         }
+        if (prev !=NULL) {
+            prev->next = NULL;
+        }
+        return head->next; 
+        // ListNode* dummy = new ListNode(0);
+        // ListNode* current = dummy;
+        // ListNode* node=head->next;
+        // while(node!=NULL){
+        //     int sum = 0;
+        //     while(node->val!=0){
+        //         sum+=node->val;
+        //         node=node->next;
+        //     }
+        //     current->next=new ListNode(sum);
+        //     current=current->next;
+        //     node=node->next;
+        // }
         
-        return dummy->next;
+        // return dummy->next;
     }
 };
