3ed7c3f99f52bf69487b1e94ffdd85553b235edb
Trisha Ghosh <122629360+Trisha2411Ghosh@users.noreply.github.com>
2024-07-10 12:10:23 +0530

    Time: 4 ms (36.8%), Space: 12.1 MB (46.41%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n =nums.size();
-        int x;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i;
-            }
-            else if(target<nums[0]){
-                return 0;
-            }
-            else if(target>nums[n-1]){
-                return n;
-            }
-            else if(target>nums[i] && target<nums[i+1]){
-                x=i+1;
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return x;
+
+        return left;
+
+        // int n =nums.size();
+        // int x;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        //     else if(target<nums[0]){
+        //         return 0;
+        //     }
+        //     else if(target>nums[n-1]){
+        //         return n;
+        //     }
+        //     else if(target>nums[i] && target<nums[i+1]){
+        //         x=i+1;
+        //     }
+        // }
+        // return x;
     }
 };
